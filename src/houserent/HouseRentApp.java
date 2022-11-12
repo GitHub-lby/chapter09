9bb55dd772f32c929be67d7515cf2e0ce2c14c26
Boyuan <1933227161@qq.com>
2022-11-12 14:21:43 +0800

    hot-fix commit1

--- src/houserent/HouseRentApp.java
+++ src/houserent/HouseRentApp.java
@@ -8,5 +8,6 @@ public class HouseRentApp {
         //创建HouseView对象，并且显示主菜单，是整个程序的入口
         new HouseView().mainMenu();
         System.out.println("====你退出房屋出租系统1.0====");
+        System.out.println("hot-fix test!");
     }
 }
